f9c7c49d559b0ce5931f8a504d653ae1ee12d0e6
Destiny <Osasehid@gmail.com>
2025-08-07 14:49:57 +0100

    project init

---
+++ PiggyBank/Profiles/AutomapperProfiles.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PiggyBank.Models;
+
+
+
+namespace PiggyBank.Profiles
+
+{
+  public class AutomapperProfiles : Profile
+  {
+    public AutomapperProfiles()
+    {
+      CreateMap<RegisterNewAccountModel, Account>();
+
+      CreateMap<UpdateAccountModel, Account>();
+      CreateMap<Account, GetAccountModel>();
+      CreateMap<TransactionRequestDto, Transaction>();
+      //mapping to dto classes
+    }
+  }
+}
---
+++ PiggyBank/Program.cs
@@ -0,0 +1,68 @@
+using PiggyBank.DAL;
+using Microsoft.OpenApi.Models;
+using PiggyBank.Services.Interfaces;
+using PiggyBank.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using PiggyBank.Utils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<PiggyBankDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+builder.Services.AddScoped<AccountService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddControllers();
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+    {
+       x.SwaggerDoc("v1", new OpenApiInfo
+      {
+        Title = "PiggyBank banking api",
+          Version = "v1",
+          Description = "Banking api with basic functionalities",
+          Contact = new OpenApiContact
+          {
+               Name = "Richard Nwonah",
+               Email = "richardnwonah@outlook.com",
+               Url = new Uri("https://github.com/richardnwonah/PiggieBank")
+    
+            }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(
+        //x =>
+    //{
+      //  var profix = string.IsNullOrEmpty(x.RouteProfile) ? "." : "..";
+       // x.SwaggerEndpoint($"{prefix}/swagger/v1/swagger.json", "PiggyBanking Api doc");
+
+    //}
+    );
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
